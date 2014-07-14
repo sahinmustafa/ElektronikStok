a3d77ceec66b440c7000310ce3079fb1c185a8bc
Mustafa-Bilgisay <Mustafa-Bilgisay@192.168.1.4>
2014-07-14 22:56:47 +0300

    Masa Otomatik Kapat

---
+++ src/elektronikstok/ElektronikStok.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elektronikstok;
+
+/**
+ *
+ * @author Mustafa-Bilgisay
+ */
+public class ElektronikStok {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
