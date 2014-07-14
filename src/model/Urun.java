879daa1d10fd6f2fd4eef6e252f899af8b9dabdb
Mustafa-Bilgisay <Mustafa-Bilgisay@192.168.1.4>
2014-07-14 23:38:34 +0300

    Urun sinifi olusturuldu

---
+++ src/model/Urun.java
@@ -0,0 +1,23 @@
+package model;
+
+/**
+ *
+ * @author MustafaSahin
+ */
+public class Urun {
+    
+    private int urunID;
+    private String adi;
+    private String ozellik;
+    private int stokMiktar;
+    private double alisFiyati;
+    private double satisFiyati;
+    private int kritikStokMiktar;
+    private Kategori kategori;
+    private String aciklama;
+    private String raf;
+    
+    public Urun(){}
+    
+    
+}
