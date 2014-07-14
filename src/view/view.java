9ac330affbcf1b66865a42a4cec6b8324c38f61b
Mustafa-Bilgisay <Mustafa-Bilgisay@192.168.1.4>
2014-07-14 23:00:08 +0300

    CreateProject

---
+++ src/view/view.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author Mustafa-Bilgisay
+ */
+public class view {
+    
+}
