349432b9ffc6f803c01e97f40504725f94cc8cc3
Mustafa-Bilgisay <Mustafa-Bilgisay@192.168.1.4>
2014-07-14 22:59:30 +0300

    CreateProject

--- src/elektronikstok/ElektronikStok.java
+++ src/elektronikstok/ElektronikStok.java
@@ -1,19 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package elektronikstok;
 
-/**
- *
- * @author Mustafa-Bilgisay
- */
 public class ElektronikStok {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
 }
